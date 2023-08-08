c375ae190bd7a948817ccd21ce6c3b2f4cc888d7
Nauffal Rizky <sazane2005@gmail.com>
2023-08-08 21:41:16 +0700

    Third Commit

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // ALL ELEMENTS NEEDED
+const menu = document.querySelector(".menu");
+const optionX = menu.querySelector(".option-x");
+const optionY = menu.querySelector(".option-o");
+const game = document.querySelector(".game");
+const players = game.querySelector(".players");
+
+window.addEventListener("DOMContentLoaded", () => {
+	menu.classList.add("show");
+	optionX.addEventListener("click", () => {
+		clickAndDisappear();
+	});
+	optionY.addEventListener("click", () => {
+		clickAndDisappear();
+		players.classList.add("active");
+	});
+});
+
+const clickAndDisappear = () => {
+	menu.classList.remove("show");
+	game.classList.add("show");
+};
