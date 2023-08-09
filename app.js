ecef0c646a4ca24324e640f9dce6e99b26959a3b
Nauffal Nur Rizky <nauffal2005@gmail.com>
2023-08-09 13:51:58 +0700

    Fourth Commit

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 // ALL ELEMENTS NEEDED
-const menu = document.querySelector(".menu");
-const optionX = menu.querySelector(".option-x");
-const optionY = menu.querySelector(".option-o");
-const game = document.querySelector(".game");
-const players = game.querySelector(".players");
+const menu = document.querySelector('.menu');
+const optionX = menu.querySelector('.option-x');
+const optionO = menu.querySelector('.option-o');
+const game = document.querySelector('.game');
+const players = game.querySelector('.players');
+const sections = game.querySelectorAll('.gameboard span');
 
-window.addEventListener("DOMContentLoaded", () => {
-	menu.classList.add("show");
-	optionX.addEventListener("click", () => {
-		clickAndDisappear();
-	});
-	optionY.addEventListener("click", () => {
-		clickAndDisappear();
-		players.classList.add("active");
-	});
+window.addEventListener('DOMContentLoaded', () => {
+  sections.forEach((section) => {
+    section.addEventListener('click', () => {
+      clickedBox(section);
+    });
+  });
+
+  menu.classList.add('show');
+  optionX.addEventListener('click', () => {
+    clickAndDisappear();
+  });
+  optionO.addEventListener('click', () => {
+    clickAndDisappear();
+    players.classList.add('active');
+  });
 });
 
 const clickAndDisappear = () => {
-	menu.classList.remove("show");
-	game.classList.add("show");
+  menu.classList.remove('show');
+  game.classList.add('show');
+};
+
+const clickedBox = (element) => {
+  if (players.classList.contains('active')) {
+    element.innerHTML = `O`;
+  } else {
+    element.innerHTML = `X`;
+  }
 };
